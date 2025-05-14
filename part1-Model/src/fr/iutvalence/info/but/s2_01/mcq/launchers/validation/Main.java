3043362f11377813c120827df0fcb7b002628b72
evgle <doisynoa@icloud.com>
2025-05-15 01:46:52 +0200

    1-Model-Configuration

--- part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/launchers/validation/Main.java
+++ part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/launchers/validation/Main.java
@@ -1,8 +1,14 @@
-package fr.iutvalence.info.but.s2_01.mcq.launchers.validation;
+package src.fr.iutvalence.info.but.s2_01.mcq.launchers.validation;
+
 
 /**
  * Preamble application, used to validate project configuration.
  */
 public class Main {
-    // To be copied/pasted from part0-Preamble
+
+    // To be completed!
+    // -> this application is supposed to write "Hello MCQ!" on standard output.
+    public static void main(String[] args) {
+        System.out.println("Hello MCQ!");
+    }
 }
