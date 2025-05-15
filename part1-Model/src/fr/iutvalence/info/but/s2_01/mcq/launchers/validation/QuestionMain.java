4f4654d0fc1e909a47f28176e75da6703f8b85f6
evgle <doisynoa@icloud.com>
2025-05-15 02:24:49 +0200

    1-Model-Question

--- part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/launchers/validation/QuestionMain.java
+++ part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/launchers/validation/QuestionMain.java
@@ -1,4 +1,6 @@
-package fr.iutvalence.info.but.s2_01.mcq.launchers.validation;
+package src.fr.iutvalence.info.but.s2_01.mcq.launchers.validation;
+
+import src.fr.iutvalence.info.but.s2_01.mcq.model.core.Question;
 
 /**
  * Question validation application.
@@ -10,6 +12,24 @@ public class QuestionMain {
      * @param args command-line arguments (unused).
      */
     public static void main(String[] args) {
-        // To be completed!
+        System.out.println("Test de toString\n");
+        Question q1 =
+                new Question("What is the answer to life, universe and everything?",
+                        new String[]{"42", "32768", "There is no answer", "Kamoulox"},
+                        0);
+        System.out.println(q1.toString());
+
+        System.out.println("\nTest de la methode getAnswer");
+        System.out.println(q1.getAnswer(5));
+        System.out.println(q1.getAnswer(2));
+
+        System.out.println("\nTest de la methode getCorrectAnswerId");
+        System.out.println(q1.getCorrectAnswerId());
+
+        System.out.println("\nTest de la methode getAnswerCount");
+        System.out.println(q1.getAnswerCount());
+
+        System.out.println("\nTest de la methode getQuestion");
+        System.out.println(q1.getQuestion());
     }
 }
--- part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/model/core/Question.java
+++ part1-Model/src/fr/iutvalence/info/but/s2_01/mcq/model/core/Question.java
@@ -1,8 +1,50 @@
-package fr.iutvalence.info.but.s2_01.mcq.model.core;
+package src.fr.iutvalence.info.but.s2_01.mcq.model.core;
+
+import java.util.Arrays;
 
 /**
  * Question of a questionnaire (question, answers, correct answer id).
  */
 public class Question  {
-    // To be completed!
+    private final String question;
+    private final String[] answers;
+    private final int correctAnswerId;
+
+    public Question(String question, String[] answers, int correctAnswerId) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswerId = correctAnswerId;
+    }
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public int getCorrectAnswerId() {
+        return this.correctAnswerId;
+    }
+
+    public String getAnswer(int answerId) {
+        if (answerId>this.getAnswerCount()-1){
+            return null;
+        }
+        return this.answers[answerId];
+    }
+
+    public int getAnswerCount(){
+        return this.answers.length;
+    }
+
+    public String toString() {
+        String result = "? -> "+this.question;
+        for (int i = 0; i<this.getAnswerCount(); i++){
+            if (this.correctAnswerId==i){
+                result+="\n"+i+" -> (o) "+this.answers[i];
+            }
+            else {
+                result += "\n"+i+" -> (x) "+this.answers[i];
+            }
+        }
+        return result;
+    }
 }
